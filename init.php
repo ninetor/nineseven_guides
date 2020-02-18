2034485a91a834ecd0138e194f9fe432ebe1e461
Nineseven <nine@nineseven.ru>
2020-02-18 14:06:28 +0300

    Create init.php

---
+++ init.php
@@ -0,0 +1,4 @@
+<?
+require_once 'include/constants.php'; // Файл с константами
+require_once 'include/functions.php'; // Файл с функциями
+require_once 'include/event_handlers.php'; // Файл событий
